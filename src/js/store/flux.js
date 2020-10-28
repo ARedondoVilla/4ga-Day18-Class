4b9483eef3f606dec39b01e84ee28392f55e3689
ARedondoVilla <alexredondovilla@gmail.com>
2020-10-28 22:20:53 +0000

    initial commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,26 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			Agenda: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
+			getAllContacts() {
+				const endpoint = "https://assets.breatheco.de/apis/fake/contact/agenda/ARedondoVilla";
+				const config = {
+					method: "GET"
+				};
+				fetch(endpoint, config)
+					.then(response => {
+						return response.json();
+					})
+					.then(json => {
+						setStore({
+							agenda: json.results
+						});
+						console.log(json.results);
+					});
+			}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
